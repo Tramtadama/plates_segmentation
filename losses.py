b46c72850837af559c76c8c8526625b49ef9c51f
Tramtadama <kakadustew@seznam.cz>
2020-05-06 23:07:18 +0200

    added f_score

--- losses.py
+++ losses.py
@@ -24,6 +24,34 @@ def iou(pr, gt, eps=1e-7, threshold=None, activation='sigmoid'):
     return (intersection + eps) / union
 
 
+def f_score(pr, gt, beta=1, eps=1e-7,
+        threshold=None, activation='sigmoid'):
+
+    if activation is None or activation == "none":
+        activation_fn = lambda x: x
+    elif activation == "sigmoid":
+        activation_fn = torch.nn.Sigmoid()
+    elif activation == "softmax2d":
+        activation_fn = torch.nn.Softmax2d()
+    else:
+        raise NotImplementedError(
+                "Activation implemented for sigmoid and softmax2d"          )
+
+    pr = activation_fn(pr)
+
+    if threshold is not None:
+        pr = (pr > threshold).float()
+
+    tp = torch.sum(gt * pr)
+    fp = torch.sum(pr) - tp
+    fn = torch.sum(gt) - tp
+
+    score = ((1 + beta ** 2) * tp + eps) \
+            / ((1 + beta ** 2) * tp + beta ** 2 * fn + fp + eps)
+
+    return score
+
+
 class DiceLoss(nn.Module):
     __name__ = 'dice_loss'
 
